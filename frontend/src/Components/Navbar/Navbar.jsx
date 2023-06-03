5187e6d00916887f81065a802e581adf78738c61
aswnss-m <aswnss.m@gmail.com>
2023-06-03 22:41:32 +0530

    buttons , navbar and passport set

--- frontend/src/Components/Navbar/Navbar.jsx
+++ frontend/src/Components/Navbar/Navbar.jsx
@@ -1,33 +1,59 @@
-import React from 'react'
-import logo from "./cap.png"
-import "./Navbar.css"
-import CapeLogo from '../CapeLogo/CapeLogo'
-// import passport from '../../Utils/passport'
+import React, { useEffect, useState } from 'react';
+import logo from "./cap.png";
+import "./Navbar.css";
+import CapeLogo from '../CapeLogo/CapeLogo';
+import { setPassport, getPassport, removePassport } from '../../Utils/passport';
+
 function Navbar() {
-    return (
-        <div className='nav'>
-            <div className="navLogo">
-                <span className='alumininetLogo'>
-                  <img src={logo}
-                      alt="cap icon"/>
-                  <p className='logoName inter-800'>AluminiNet</p>
-                </span>
-                <CapeLogo/>
-            </div>
-            <div className="navLinks">
-                <span class="material-symbols-rounded">
-                    description
-                </span>
-                <span class="material-symbols-rounded">
-                    groups
-                </span>
-            </div>
-            <div className="navButtons">
-              <button className='secondaryButtton'>Join now</button>
-              <button className='primaryButton'>Sign in</button>
-            </div>
+  // handle navbar change for sign in and signs out
+  const [loggedIn, setLoggedIn] = useState(false);
+
+  useEffect(() => {
+    const passport = getPassport();
+    setLoggedIn(passport);
+  }, []);
+
+  const handleSignIn = () => {
+    setPassport(true);
+  };
+
+  const handleLogOut = () => {
+    removePassport();
+    setLoggedIn(false);
+  };
+
+  return (
+    <div className='nav'>
+      <div className="navLogo">
+        <span className='alumininetLogo'>
+          <img src={logo} alt="cap icon" />
+          <p className='logoName inter-800'>AluminiNet</p>
+        </span>
+        <CapeLogo />
+      </div>
+      <div className="navLinks">
+        <span className="material-symbols-rounded">
+          description
+        </span>
+        <span className="material-symbols-rounded">
+          groups
+        </span>
+      </div>
+      {!loggedIn ? (
+        <div className="navButtons">
+          <button className='secondaryButtton'>Join now</button>
+          <button className='primaryButton' onClick={handleSignIn}>Sign In</button>
         </div>
-    )
+      ) : (
+        <div className="navButtons">
+          <span className="material-symbols-rounded">
+            person
+          </span>
+          <button className='primaryButton' onClick={handleLogOut}>Log out</button>
+        </div>
+      )}
+    </div>
+  );
 }
 
-export default Navbar
+export default Navbar;
--- frontend/src/Utils/passport.js
+++ frontend/src/Utils/passport.js
@@ -1,8 +1,9 @@
-export default function passport() {
-    const token = localStorage.getItem("token");
-    if (token) {
-        return true;
-    } else {
-        return false;
-    }
-    }
+export function setPassport(passport) {
+    localStorage.setItem('token', passport);
+}
+export function getPassport() {
+    return localStorage.getItem('token');
+}
+export function removePassport() {
+    localStorage.removeItem('token');
+}
