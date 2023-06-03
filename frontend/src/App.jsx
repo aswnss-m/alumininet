2c4ebc37ea4abb276f555cb02aeb513ec095b240
aswnss-m <aswnss.m@gmail.com>
2023-06-04 00:50:09 +0530

    initial setup

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import Navbar from './Components/Navbar/Navbar'
+import "./Assets/App.css"
 function App() {
+  // localStorage.setItem('token', '1234567890')
   return (
     <div>
       <Navbar />
