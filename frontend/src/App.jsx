23ff93e1ce4aa221b1ee3e85bb30fbb4e0c708fc
aswnss-m <aswnss.m@gmail.com>
2023-06-04 07:47:24 +0530

    routing path added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,16 @@
 import React from 'react'
 import Navbar from './Components/Navbar/Navbar'
 import "./Assets/App.css"
+import { Routes , Route } from 'react-router-dom' 
+import Landing from './Pages/Landing/Landing'
 function App() {
   // localStorage.setItem('token', '1234567890')
   return (
     <div>
       <Navbar />
+      <Routes>
+        <Route path={'/'} element={<Landing />}/>
+      </Routes>
     </div>
   )
 }
