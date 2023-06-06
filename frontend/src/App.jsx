ef802e8ce9c71db8d994b9a941a21140ebeeacc3
aswnss-m <aswnss.m@gmail.com>
2023-06-06 09:01:31 +0530

    re routing on login

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,27 +1,38 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
 import Navbar from './Components/Navbar/Navbar'
 import "./Assets/App.css"
-import { Routes , Route } from 'react-router-dom' 
+import {Routes, Route} from 'react-router-dom'
 import Landing from './Pages/Landing/Landing'
 import Register from './Pages/Register/Register'
 import Login from './Pages/Login/Login'
 import Home from './Pages/Home/Home'
 import BlogPage from './Pages/BlogPage/BlogPage'
+import {getPassport} from './Utils/passport'
 function App() {
-  // localStorage.setItem('token', '1234567890')
-  return (
-    <div>
-      <Navbar />
-      <Routes>
-        <Route path={'/'} element={<Landing />}/>
-        <Route path={'/register'} element={<Register />}/>
-        <Route path={'/login'} element={<Login />}/>
-        {/* Set the following routes to show only if logged in */}
-        <Route path={'/home'} element={<Home />}/>
-        <Route path={'/blog/:id'} element={<BlogPage />} />
-      </Routes>
-    </div>
-  )
+    const [loggedIn, setLoggedIn] = useState(getPassport());
+    useEffect(() => {
+        setLoggedIn(getPassport());
+    }, [])
+    return (
+        <div>
+            <Navbar/>
+            <Routes> {
+                !loggedIn ? <>
+                    <Route path={'/'}
+                        element={<Landing/>}/>
+                    <Route path={'/register'}
+                        element={<Register/>}/>
+                    <Route path={'/login'}
+                        element={<Login/>}/>
+                </> : <>
+                    <Route path={'/'}
+                        element={<Home/>}/>
+                    <Route path={'/blog/:id'}
+                        element={<BlogPage/>}/>
+                </>
+            } </Routes>
+        </div>
+    )
 }
 
 export default App
