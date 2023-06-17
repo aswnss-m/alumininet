b1b3d0b3ca0aac03434a9c5b85fceacbcedf520e
aswnss-m <aswnss.m@gmail.com>
2023-06-17 13:06:10 +0530

    get user by id

--- backend/routes/alumini.js
+++ backend/routes/alumini.js
@@ -3,6 +3,14 @@ const router = express.Router();
 const User = require('../Models/User.model');
 
 
+//Route to get one user by id
+router.route('/:id').get((req, res) => {
+    User.findById(req.params.id, "-password")
+      .then(user => {
+        res.json(user)})
+      .catch(err => res.status(400).json('Error: ' + err));
+  });
+  
 // Route to get users with "type" value true
 router.route('/all').get((req, res) => {
     User.find({ type: true })
